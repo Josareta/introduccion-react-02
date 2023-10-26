45b15aaeae301ac32805853290b2c2d5b21a3d1c
Josareta <josareta@gmail.com>
2023-10-26 15:24:15 +0200

    Añadir Ejercicio 03

---
+++ src/components/Ejercicio3/Ejercicio3.jsx
@@ -0,0 +1,14 @@
+
+import TituloEjercicio from '../TituloEjercicio'
+import Navegacion from "./Navegacion"
+
+
+export default function Ejercicio3() {
+  return (
+    <section className="caja-ejercicio">
+      <TituloEjercicio>Este es el ejercicio 3</TituloEjercicio>
+      <Navegacion />
+      
+    </section>
+  )
+}
